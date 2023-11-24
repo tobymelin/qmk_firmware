76f68e5e7bae67bf8101b147df9c0989ab42f0d3
Toby M. <n.tobias.melin@gmail.com>
2023-11-24 14:47:40 +0000

    Change config to allow for split space, add combos for brackets

--- keyboards/planck/rev7/keymaps/tobymelin/keymap.c
+++ keyboards/planck/rev7/keymaps/tobymelin/keymap.c
@@ -37,22 +37,34 @@ const uint32_t unicode_map[] PROGMEM = {
     [OE]  = 0x00D6,
 };
 
+const uint16_t PROGMEM left_bracket_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM right_bracket_combo[] = {KC_K, KC_L, COMBO_END};
+const uint16_t PROGMEM left_paren_combo[] = {KC_M, KC_COMM, COMBO_END};
+const uint16_t PROGMEM right_paren_combo[] = {KC_COMM, KC_DOT, COMBO_END};
+
+combo_t key_combos[] = {
+    COMBO(left_bracket_combo, KC_LBRC),
+    COMBO(right_bracket_combo, KC_RBRC),
+    COMBO(left_paren_combo, KC_LPRN),
+    COMBO(right_paren_combo, KC_RPRN),
+};
+
 /* clang-format off */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//        ┌─────────────┬──────┬──────┬──────┬───────┬──────┐   ┌────────────────┬───────┬──────┬──────┬────┬──────┐
-//        │     tab     │  q   │  w   │  e   │   r   │  t   │   │       y        │   u   │  i   │  o   │ p  │ bspc │
-//        ├─────────────┼──────┼──────┼──────┼───────┼──────┤   ├────────────────┼───────┼──────┼──────┼────┼──────┤
-//        │ LCTL_T(esc) │  a   │  s   │  d   │   f   │  g   │   │       h        │   j   │  k   │  l   │ ;  │  '   │
-//        ├─────────────┼──────┼──────┼──────┼───────┼──────┤   ├────────────────┼───────┼──────┼──────┼────┼──────┤
-//        │    lsft     │  z   │  x   │  c   │   v   │  b   │   │       n        │   m   │  ,   │  .   │ /  │ ent  │
-//        ├─────────────┼──────┼──────┼──────┼───────┼──────┤   ├────────────────┼───────┼──────┼──────┼────┼──────┤
-//        │    lctl     │ rgui │ lalt │ lgui │ LOWER │ rsft │   │ LT(_NUMS, spc) │ RAISE │ left │ down │ up │ rght │
-//        └─────────────┴──────┴──────┴──────┴───────┴──────┘   └────────────────┴───────┴──────┴──────┴────┴──────┘
+//        ┌─────────────┬──────┬──────┬──────┬───────┬────────────────┐   ┌──────┬───────┬──────┬──────┬────┬──────┐
+//        │     tab     │  q   │  w   │  e   │   r   │       t        │   │  y   │   u   │  i   │  o   │ p  │ bspc │
+//        ├─────────────┼──────┼──────┼──────┼───────┼────────────────┤   ├──────┼───────┼──────┼──────┼────┼──────┤
+//        │ LCTL_T(esc) │  a   │  s   │  d   │   f   │       g        │   │  h   │   j   │  k   │  l   │ ;  │  '   │
+//        ├─────────────┼──────┼──────┼──────┼───────┼────────────────┤   ├──────┼───────┼──────┼──────┼────┼──────┤
+//        │    lsft     │  z   │  x   │  c   │   v   │       b        │   │  n   │   m   │  ,   │  .   │ /  │ ent  │
+//        ├─────────────┼──────┼──────┼──────┼───────┼────────────────┤   ├──────┼───────┼──────┼──────┼────┼──────┤
+//        │    lctl     │ rgui │ lalt │ lgui │ LOWER │ LT(_NUMS, spc) │   │ bspc │ RAISE │ left │ down │ up │ rght │
+//        └─────────────┴──────┴──────┴──────┴───────┴────────────────┘   └──────┴───────┴──────┴──────┴────┴──────┘
 [_QWERTY] = LAYOUT_planck_grid(
-      KC_TAB         , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T    ,     KC_Y              , KC_U  , KC_I    , KC_O    , KC_P    , KC_BSPC,
-      LCTL_T(KC_ESC) , KC_A    , KC_S    , KC_D    , KC_F  , KC_G    ,     KC_H              , KC_J  , KC_K    , KC_L    , KC_SCLN , KC_QUOT,
-      KC_LSFT        , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B    ,     KC_N              , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT ,
-      KC_LCTL        , KC_RGUI , KC_LALT , KC_LGUI , LOWER , KC_RSFT ,     LT(_NUMS, KC_SPC) , RAISE , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
+      KC_TAB         , KC_Q    , KC_W    , KC_E    , KC_R  , KC_T              ,     KC_Y    , KC_U  , KC_I    , KC_O    , KC_P    , KC_BSPC,
+      LCTL_T(KC_ESC) , KC_A    , KC_S    , KC_D    , KC_F  , KC_G              ,     KC_H    , KC_J  , KC_K    , KC_L    , KC_SCLN , KC_QUOT,
+      KC_LSFT        , KC_Z    , KC_X    , KC_C    , KC_V  , KC_B              ,     KC_N    , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , KC_ENT ,
+      KC_LCTL        , KC_RGUI , KC_LALT , KC_LGUI , LOWER , LT(_NUMS, KC_SPC) ,     KC_BSPC , RAISE , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
 ),
 
 //        ┌─────┬───────┬─────┬─────┬─────┬─────┐   ┌──────┬──────┬──────┬──────┬──────┬──────┐
@@ -88,18 +100,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 ),
 
 //        ┌─────┬─────┬─────┬─────┬─────┬─────┐   ┌──────┬──────┬─────┬──────┬───────┬───────┐
-//        │  `  │     │  7  │  8  │  9  │  .  │   │      │  [   │  ]  │  =   │   [   │ bspc  │
+//        │  `  │     │  7  │  8  │  9  │  -  │   │      │  [   │  ]  │  =   │   [   │ bspc  │
 //        ├─────┼─────┼─────┼─────┼─────┼─────┤   ├──────┼──────┼─────┼──────┼───────┼───────┤
 //        │     │     │  4  │  5  │  6  │  0  │   │ left │ down │ up  │ rght │ X(AE) │ X(OE) │
 //        ├─────┼─────┼─────┼─────┼─────┼─────┤   ├──────┼──────┼─────┼──────┼───────┼───────┤
-//        │     │     │  1  │  2  │  3  │  -  │   │      │  {   │  }  │      │       │       │
+//        │     │     │  1  │  2  │  3  │  .  │   │      │  {   │  }  │      │       │       │
 //        ├─────┼─────┼─────┼─────┼─────┼─────┤   ├──────┼──────┼─────┼──────┼───────┼───────┤
 //        │     │     │     │     │     │     │   │      │      │     │      │       │       │
 //        └─────┴─────┴─────┴─────┴─────┴─────┘   └──────┴──────┴─────┴──────┴───────┴───────┘
 [_NUMS] = LAYOUT_planck_grid(
-      KC_GRV  , _______ , KC_7    , KC_8    , KC_9    , KC_DOT  ,     _______ , KC_LBRC , KC_RBRC , KC_EQL  , KC_LBRC , KC_BSPC,
+      KC_GRV  , _______ , KC_7    , KC_8    , KC_9    , KC_MINS ,     _______ , KC_LBRC , KC_RBRC , KC_EQL  , KC_LBRC , KC_BSPC,
       _______ , _______ , KC_4    , KC_5    , KC_6    , KC_0    ,     KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , X(AE)   , X(OE)  ,
-      _______ , _______ , KC_1    , KC_2    , KC_3    , KC_MINS ,     _______ , KC_LCBR , KC_RCBR , _______ , _______ , _______,
+      _______ , _______ , KC_1    , KC_2    , KC_3    , KC_DOT  ,     _______ , KC_LCBR , KC_RCBR , _______ , _______ , _______,
       _______ , _______ , _______ , _______ , _______ , _______ ,     _______ , _______ , _______ , _______ , _______ , _______
 ),
 
